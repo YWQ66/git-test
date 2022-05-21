5d65592152aa200b72df0bc050cb05284c3c7d40
YWQ <2492330849@qq.com>
2022-05-21 11:43:20 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author YWQ
+ * @create 2022/5/21-11:39
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
